5076ea4975d706e815871f3790614714a8dd8612
Maxime Tricoire <max.tricoire@gmail.com>
2015-03-25 12:43:25 +0100

    add rooms handling by path

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ws-broadcast
-Broadcast messages from a client to every other connected client (without echoing to the sender)
+Broadcast messages from a client to every other connected client (without echoing to the sender)  
+
+### Handles rooms
+You can split clients into groups by connecting them to a different path
+You just have to append a `/path` to the server url:
+
+ws://localhost:9001/myRoom
--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 var WebSocket = require('ws');
 var uuid = require('uuid');
 
-var wss = new WebSocket.Server({ port: 9001 });
+var port = process.argv[2] || 9001;
+
+var wss = new WebSocket.Server({ port: port });
+var rooms = {};
+
 wss.on('connection', function (ws) {
 	ws.id = uuid();
+	var room = rooms[ws.upgradeReq.url];
+	if (!room) {
+		room = {};
+		rooms[ws.upgradeReq.url] = room;
+	}
+	room[ws.id] = ws;
+
 	ws.on('message', function (msg) {
-		wss.clients.forEach(function (client) {
-			if (client.id !== ws.id) {
-				client.send(msg);
+		var room = rooms[ws.upgradeReq.url];
+		Object.keys(room).forEach(function (id) {
+			if (id !== ws.id) {
+				room[id].send(msg);
 			}
 		});
 	});
+
+	ws.on('close', function () {
+		delete rooms[ws.upgradeReq.url][ws.id];
+	})
 });
-console.log('WebSocket broadcaster listenning on 0.0.0.0:9001');
+
+console.log('WebSocket broadcaster listening on 0.0.0.0:'+port);
 
 module.exports = wss;
