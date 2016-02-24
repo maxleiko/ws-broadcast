0c76d514ec9f7e06103bbafbb013f8af03345e00
Maxime Tricoire <max.tricoire@gmail.com>
2016-02-24 16:52:05 +0100

    meta viewport + PROD flag for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,37 @@
 var webpack = require('webpack');
 var path = require('path');
 
+var plugins = [
+	new webpack.optimize.OccurenceOrderPlugin(),
+	new webpack.NoErrorsPlugin()
+];
+
+if (process.env.PROD) {
+	plugins = plugins.concat(
+		new webpack.optimize.UglifyJsPlugin({
+			mangle: {
+				except: ['$super', '$', 'exports', 'require']
+			}
+		}))
+}
+
 module.exports = {
-  debug: true,
-  context: path.join(__dirname, 'app'),
-  entry: './main',
-  output: {
-    path: path.join(__dirname, 'public', 'script'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules)/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015']
-      }
-    }]
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+	debug: true,
+	context: path.join(__dirname, 'app'),
+	entry: './main',
+	output: {
+		path: path.join(__dirname, 'public', 'script'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			exclude: /(node_modules)/,
+			loader: 'babel',
+			query: {
+				presets: ['react', 'es2015']
+			}
+		}]
+	},
+	plugins: plugins
 };
